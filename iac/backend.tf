183fa8f68a0fccf7a1cce742da5bf15c436667ab
daniabadin <daniabadin@gmail.com>
2022-03-31 14:40:13 +0200

    Fix TF resources

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,21 +1,7 @@
-resource "azurerm_storage_account" "tf_backend" {
-  name                     = "tfbackend"
-  resource_group_name      = "uni-labs"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = "tfbackend"
-  container_access_type = "blob"
-}
-
 terraform {
   backend "azurerm" {
       resource_group_name  = "uni-labs"
-      storage_account_name = "tfbackend"
+      storage_account_name = "unilabstfbackend"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
