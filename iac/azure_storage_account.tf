c5b76c9e8e83c82f5665a6603ab6cfeeb3b62d84
daniabadin <daniabadin@gmail.com>
2022-03-31 16:38:04 +0200

    Fix TF resources, workflow

--- iac/azure_storage_account.tf
+++ iac/azure_storage_account.tf
@@ -9,12 +9,14 @@ resource "azurerm_storage_account" "unilabscsvs" {
   name                     = "unilabscsvs"
   resource_group_name      = var.resource_group_name
   location                 = var.location
+  account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "csvs" {
   name                  = "csvs"
   storage_account_name  = azurerm_storage_account.unilabscsvs.name
-  container_access_type = "blob"
+  container_access_type = "private"
 }
