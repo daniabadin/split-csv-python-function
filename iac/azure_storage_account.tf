f6aea764b5db8ce23a11beccc2b4e7f9dd824002
daniabadin <daniabadin@gmail.com>
2022-04-01 08:47:03 +0200

    Upload TF resources

--- iac/azure_storage_account.tf
+++ iac/azure_storage_account.tf
@@ -12,6 +12,7 @@ resource "azurerm_storage_account" "unilabscsvs" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
+  allow_blob_public_access = true
 }
 
 resource "azurerm_storage_container" "csvs" {
