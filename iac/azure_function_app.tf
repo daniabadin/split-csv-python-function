f0690921841950b0165f678162a3232d41e9cefb
daniabadin <daniabadin@gmail.com>
2022-04-01 09:12:37 +0200

    Upload TF resources

--- iac/azure_function_app.tf
+++ iac/azure_function_app.tf
@@ -15,10 +15,10 @@ resource "azurerm_function_app" "split_csv_python_func" {
   os_type                    = "linux"
 
   app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "python"
-    FUNCTION_APP_EDIT_MODE   = "readonly"
+    FUNCTIONS_WORKER_RUNTIME       = "python"
+    FUNCTION_APP_EDIT_MODE         = "readonly"
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.split_csv_python_insights.instrumentation_key
-    unilabscsvs_STORAGE      = azurerm_storage_account.unilabscsvs.primary_connection_string
-    
+    unilabscsvs_STORAGE            = azurerm_storage_account.unilabscsvs.primary_connection_string
+
   }
 }
